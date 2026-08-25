d49ea1721fa4193a1a117c5b31e353a9c36e186b
jvishakaa-debug <jvishakaa@gmail.com>
2026-08-25 10:35:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.json());
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Running'));
+app.post('/api/order', (req, res) => {
+  console.log(req.body);
+  res.json({ success: true, message: "Order placed!" });
+});
+
+module.exports = app;
