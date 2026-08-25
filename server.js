013ac1114c2a9b851bef031592d37034a1f17413
jvishakaa-debug <jvishakaa@gmail.com>
2026-08-25 10:10:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = 3000;
 
 app.use(express.static(__dirname));
 
@@ -12,6 +12,8 @@ app.get('/api/products', (req, res) => {
   ]);
 });
 
-app.listen(PORT, () => {
-  console.log(`SoleStyle server running at http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+module.exports = app;
