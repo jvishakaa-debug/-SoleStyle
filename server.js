096ff254d8a4ad43d0e9fc2e29350b9058f592b0
jvishakaa-debug <jvishakaa@gmail.com>
2026-08-25 10:27:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname));
-
-app.get('/api/products', (req, res) => {
-  res.json([
-    { id: 1, name: "Men's Formal Shoe", price: 1899 },
-    { id: 2, name: "Trendy Sneaker", price: 1499 },
-    { id: 3, name: "Running Shoe", price: 2199 }
-  ]);
-});
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Running'));
